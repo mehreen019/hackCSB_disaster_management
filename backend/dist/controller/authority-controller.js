5b3ad1bf6d45622f5816114ae4b390fb73cd2e8d
sectumsemprra <jannatulrakhi371@gmail.com>
2024-07-13 22:55:12 +0600

    login and sign up works ,logout add korle same credentials diye abar log in kora jabe

--- backend/dist/controller/authority-controller.js
+++ backend/dist/controller/authority-controller.js
@@ -1,2 +1,86 @@
-export {};
+import { Authority } from "../models/authority.js";
+import { Admin } from "../models/admin.js";
+import { hash, compare } from "bcrypt";
+import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
+export const getAllAuthority = async (req, res, next) => {
+    try {
+        const authority = await Authority.find();
+        console.log("Function called");
+        return res.status(200).json({ message: "ok", authority });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "Error" });
+    }
+};
+export const authoritySignup = async (req, res, next) => {
+    try {
+        const { username, email, password } = req.body;
+        const existingAdmin = await Admin.findOne({ email });
+        const existingAuthority = await Authority.findOne({ email });
+        if (existingAdmin || existingAuthority)
+            return res.status(401).send(" Already registered");
+        const hashedPassword = await hash(password, 10);
+        const authority = new Authority({
+            username: username,
+            email: email,
+            password: hashedPassword,
+            role: 'authority'
+        });
+        await authority.save();
+        res.clearCookie(COOKIE_NAME, {
+            domain: "localhost",
+            httpOnly: true,
+            signed: true,
+            path: "/",
+        });
+        const token = createToken(authority._id.toString(), authority.email, "7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7);
+        res.cookie(COOKIE_NAME, token, { path: "/", domain: "localhost", expires,
+            httpOnly: true,
+            signed: true,
+        });
+        console.log("Function called");
+        return res.status(200).json({ message: "ok", role: authority.role });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "Error" });
+    }
+};
+export const authorityLogin = async (req, res, next) => {
+    try {
+        //user login
+        const { email, password } = req.body;
+        const authority = await Authority.findOne({ email });
+        if (!authority) {
+            return res.status(401).send("Authority not registered");
+        }
+        const isPasswordCorrect = await compare(password, authority.password);
+        if (!isPasswordCorrect) {
+            return res.status(403).send("Incorrect Password");
+        }
+        // create token and store cookie
+        res.clearCookie(COOKIE_NAME, {
+            domain: "localhost",
+            httpOnly: true,
+            signed: true,
+            path: "/",
+        });
+        const token = createToken(authority._id.toString(), authority.email, "7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7);
+        res.cookie(COOKIE_NAME, token, { path: "/", domain: "localhost", expires,
+            httpOnly: true,
+            signed: true,
+        });
+        return res.status(200).json({ message: "OK", name: authority.username, email: authority.email });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+};
 //# sourceMappingURL=authority-controller.js.map
--- backend/src/routes/authority-routes.ts
+++ backend/src/routes/authority-routes.ts
@@ -1,3 +1,11 @@
 import {Router} from "express";
+
+import { loginValidator, signupValidator, validate } from "../utils/validators.js";
+import { authorityLogin, authoritySignup , getAllAuthority} from "../controller/authority-controller.js";
+
+
 const authorityRoutes = Router();
+authorityRoutes.get("/",getAllAuthority);
+authorityRoutes.post("/signup",validate(signupValidator),authoritySignup);
+authorityRoutes.post("/login",validate(loginValidator),authorityLogin);
 export default authorityRoutes;
