b89dd0af9b853d30e0bdd80ca3f4db18094ff230
potate4 <sumaiyaahmed@iut-dhaka.edu>
2024-07-16 13:17:58 +0600

    changed button options

--- weather-app-chart/src/components/Header.js
+++ weather-app-chart/src/components/Header.js
@@ -60,17 +60,20 @@ const Header = () => {
                 onClose={handleLoginClose}
               >
                 <MenuItem onClick={handleLoginClose}>
+                  <NavigationLink to="/admin_user/login" text="User Login" />
+                </MenuItem>
+                <MenuItem onClick={handleLoginClose}>
                   <NavigationLink to="/admin_user/login" text="Admin Login" />
                 </MenuItem>
                 <MenuItem onClick={handleLoginClose}>
-                  <NavigationLink to="/authority_user/login" text="Authority Login" />
+                  <NavigationLink to="/admin_user/login" text="Authority Login" />
                 </MenuItem>
               </Menu>
               <Button
                 onClick={handleSignupClick}
                 sx={{ bgcolor: "#51538f", color: "white" }}
               >
-                Signup
+              Signup
               </Button>
               <Menu
                 anchorEl={signupAnchorEl}
@@ -78,7 +81,7 @@ const Header = () => {
                 onClose={handleSignupClose}
               >
                 <MenuItem onClick={handleSignupClose}>
-                  <NavigationLink to="/admin_user/signup" text="Admin Signup" />
+                  <NavigationLink to="/admin_user/signup" text="User Signup" />
                 </MenuItem>
                 <MenuItem onClick={handleSignupClose}>
                   <NavigationLink to="/authority_user/signup" text="Authority Signup" />
